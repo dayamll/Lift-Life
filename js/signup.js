a5aed542ce815e032ce3440466effc965a49fec1
dayamll <dayamllerenacastro@gmail.com>
2018-01-05 08:21:57 -0500

    cambiando flags

--- js/signup.js
+++ js/signup.js
@@ -1,9 +1,16 @@
 $(document).ready(function() {
   var $phoneNumber = $('.input-phone');
-  var $btn = $('.next');
+  var $btn = $('.next, .resend');
+  var $code = $('.img-flags');
+
+  // codigo de pais 
+  $code.on('click', function(event) {
+    $('#code-countries img').attr('src', '../assets/images/ban-' + $(event.target).closest('a').attr('class'));
+    $('#number').val($(event.target).closest('a').attr('data-code'));
+  });
 
   // funcion que habilita el boton NEXT,
-  // si el vlor ingresado cumple con la condicion.
+  // si el valor ingresado cumple con la condicion.
   $phoneNumber.keyup(function() {
     var $value = $(this).val();
 
@@ -13,9 +20,9 @@ $(document).ready(function() {
       $btn.attr('disabled', 'disabled');
     }
   });
-  
+
   // funcion que envia un codigo random y 
-  // direcciona a la sguiente pagina.
+  // direcciona a la siguiente pagina.
   $btn.on('click', function(event) {
     event.preventDefault();
     var $number = Math.floor(Math.random() * 1001);
